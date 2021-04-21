1c283947057a01fa9744e4e244d1cd1018dedc32
Supratim <supratim@elearnmarkets.com>
2021-04-21 18:15:31 +0530

    gifted chat

--- src/screens/ChatPage.tsx
+++ src/screens/ChatPage.tsx
@@ -1,15 +1,23 @@
 import React, {FC, useEffect, useState} from 'react';
-import {View, StyleSheet,Text} from 'react-native';
+import {View, StyleSheet, Text, Dimensions} from 'react-native';
+import database from '@react-native-firebase/database';
+import {Button, TextInput} from 'react-native-paper';
+import uuid from 'react-native-uuid';
+import {COLLECTION_NAME} from '../consts';
 import {GiftedChat} from 'react-native-gifted-chat';
+
+const {width, height} = Dimensions.get('window');
+const Devicewidth = Dimensions.get('window').width;
+const Deviceheight = Dimensions.get('window').height;
 
 export interface User {
   _id: string | number;
   name?: string;
-  avatar?: string | number;
+  avatar?: string;
 }
 
 export interface IMessage {
-  _id: string | number;
+  _id: string;
   text: string;
   createdAt: Date | number;
   user: User;
@@ -21,29 +29,38 @@ export interface IMessage {
   received?: boolean;
   pending?: boolean;
 }
-
 const ChatPage: FC = () => {
-  const [messages, setMessages] = useState<any>([]);
+  const [messages, setMessages] = useState<Array<IMessage>>([]);
+
+  function writeUserData(text: Array<IMessage>) {
+    const msg: IMessage = text[0];
+    const chatId = msg._id;
+    database().ref(`${COLLECTION_NAME.CHATS}/${chatId}`).set(msg);
+  }
 
   useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: 'Hello developer',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-      },
-    ]);
+    database()
+      .ref(COLLECTION_NAME.CHATS)
+      .on('value', function (snapshot: any) {
+        let tempMsg: Array<IMessage> = [];
+        snapshot.forEach((childSnapshot: any) => {
+          var childData = childSnapshot.val();
+          tempMsg.push(childData);
+        });
+        setMessages(tempMsg);
+      });
   }, []);
 
-  return <View style={styles.root}>
-
-    <Text>ChatPage</Text>
-  </View>;
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={messages => writeUserData(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
+  );
+ 
 };
 
 export default ChatPage;
@@ -53,5 +70,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     flex: 1,
     width: '100%',
+  },
+  loginText: {
+    color: '#F5F5F5',
+  },
+  loginBtn: {
+    height: 55,
+    width: Devicewidth / 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#E5566D',
+    borderRadius: 10,
   },
 });
